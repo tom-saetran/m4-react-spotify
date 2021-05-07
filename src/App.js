00955e0a8180882e6238aaa90f34aa5789e299dd
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-07 14:33:38 +0200

    -

--- src/App.js
+++ src/App.js
@@ -10,17 +10,26 @@ import MediaControl from "./components/MediaControl"
 import PageNotFound from "./components/PageNotFound"
 
 class App extends React.Component {
+    state = {
+        nowPlaying: null,
+        loggedInUser: null
+    }
+
+    setNowPlaying = id => {
+        console.log(id)
+    }
+
     render() {
         return (
             <Router>
-                <NavLeft />
+                <NavLeft target={this.state.loggedInUser} />
                 <Switch>
-                    <Route component={Home} exact path="/" />
-                    <Route component={Artist} exact path="/artist/:id" />
-                    <Route component={Album} exact path="/album/:id" />
-                    <Route component={PageNotFound} />
+                    <Route render={routerProps => <Home setNowPlaying={id => this.setNowPlaying(id)} {...routerProps} />} exact path="/" />
+                    <Route render={routerProps => <Artist setNowPlaying={id => this.setNowPlaying(id)} {...routerProps} />} exact path="/artist/:id" />
+                    <Route render={routerProps => <Album setNowPlaying={id => this.setNowPlaying(id)} {...routerProps} />} exact path="/album/:id" />
+                    <Route render={routerProps => <PageNotFound {...routerProps} />} />
                 </Switch>
-                <MediaControl />
+                <MediaControl setNowPlaying={id => this.setNowPlaying(id)} target={this.state.nowPlaying} />
             </Router>
         )
     }
