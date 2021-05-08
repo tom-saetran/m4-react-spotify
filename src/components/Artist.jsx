c827aeb7963364ff02f2ad531c8bdc1ff4bcf6f2
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-08 18:15:25 +0200

    -

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Spinner from "react-bootstrap/Spinner"
+import Image from "react-bootstrap/Image"
 
 class Artist extends React.Component {
     state = {
@@ -17,29 +18,38 @@ class Artist extends React.Component {
         return (
             <div className="mainPage">
                 <div className="row justify-content-center">
-                    <div className="col-12 col-md-10 col-lg-10 mt-5">
-                        <h2 className="text-muted text-center">{this.state.artistData.name}</h2>
-                        <div className="text-muted text-center">
-                            {this.state.artistData.nb_fan} follower{this.state.artistData.nb_fan === 1 ? "" : "s"}
+                    {this.state.artistData.nb_fan !== undefined ? (
+                        <div className="col-12 pt-5">
+                            <img className="rounded-circle center" src={this.state.artistData.picture} alt={this.state.artistData.name} />
+                            <h2 className="text-muted text-center pt-2">{this.state.artistData.name}</h2>
+                            <div className="text-muted text-center">
+                                {this.state.artistData.nb_fan} follower{this.state.artistData.nb_fan === 1 ? "" : "s"}
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className="pt-5 text-center">
+                            <Spinner animation="border" variant="secondary" />
+                        </div>
+                    )}
                 </div>
 
-                {this.state.artistData.nb_album > 0 && (
+                {this.state.artistData.nb_album > 0 ? (
                     <div className="row justify-content-center mb-3">
-                        <div className="col-10 col-md-10 col-lg-10 p-0">
+                        <div className="col-11 p-0">
                             <div className="mt-4 d-flex justify-content-start">
                                 <h2 className="text-muted font-weight-bold">Albums</h2>
                             </div>
-                            <div className="pt-5 mb-5">
-                                <div className="row">
+                            <div className="mb-5">
+                                <div className="row pb-5">
                                     {this.state.albumsData.data === undefined ? (
-                                        <Spinner animation="border" variant="primary" />
+                                        <div className="text-center pt-5">
+                                            <Spinner animation="border" variant="secondary" />
+                                        </div>
                                     ) : (
                                         this.state.albumsData.data.map(album => (
                                             <div key={album.id} className="col-sm-auto col-md-auto mb-3">
                                                 <Link to={"/album/" + album.id}>
-                                                    <img className="img-fluid" src={album.cover} alt={album.title} />
+                                                    <Image fluid src={album.cover} alt={album.title} />
                                                 </Link>
                                                 <p className="text-muted fit-content">{album.title}</p>
                                             </div>
@@ -49,6 +59,10 @@ class Artist extends React.Component {
                             </div>
                         </div>
                     </div>
+                ) : this.state.artistData.nb_album !== undefined ? (
+                    <h2 className="text-muted text-center">No Albums</h2>
+                ) : (
+                    ""
                 )}
             </div>
         )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,20 +23,25 @@ class Home extends React.Component {
                         <div style={{ display: this.state.data === null ? "none" : "" }}>
                             <h2 className="text-center text-muted pt-3">Search Results</h2>
                             {this.state.data !== null && this.state.data.data.length === 0 && <p className="text-center text-dim">No results</p>}
-                            <div className="row pt-5">
+                            <div className="row py-5">
                                 {this.state.data !== null &&
                                     this.state.data.data.map(result => (
                                         <div key={result.id} className="col-sm-auto col-md-auto mb-3 text-dim py-2">
                                             <Link to={"/album/" + result.album.id}>
                                                 <img src={result.album.cover} alt={result.album.id} />
                                             </Link>
-                                            <Link to={"/artist/" + result.artist.id}>
-                                                <p className="fit-content">{result.artist.name}</p>
+                                            <Link className="text-dim no-underline" to={"/artist/" + result.artist.id}>
+                                                <h4 className="fit-content m-0">{result.artist.name}</h4>
                                             </Link>
-                                            <p className="fit-content">{result.album.title}</p>
-                                            <p className="fit-content">{result.title}</p>
+                                            <h6 className="fit-content m-0">
+                                                <small>{result.album.title}</small>
+                                            </h6>
+                                            <p className="fit-content m-0">
+                                                <small>{result.title}</small>
+                                            </p>
                                         </div>
                                     ))}
+                                <div className="pb-5" />
                             </div>
                         </div>
                         {this.state.data === null && <FontAwesomeIcon className="w-100 text-muted mt-5" size="5x" icon="sync" spin />}
